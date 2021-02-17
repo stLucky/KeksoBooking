e03197c374c31b5d52393ec12e071c2fb27a3891
stLucky <asys4759@gmail.com>
2021-02-17 09:49:22 +0300

    вынес функции отрисовки фрагментов пункта списка и фото за итератор, функцию переводчик в утилитарные

--- js/popup.js
+++ js/popup.js
@@ -1,23 +1,43 @@
 import { ads } from './data.js';
-import { getDeclinedWord } from './util.js';
+import { getDeclinedWord, translateHousingType } from './util.js';
+
 
 const mapCanvas = document.querySelector('#map-canvas');
 const cardAdTemplate = document.querySelector('#card').content.querySelector('.popup');
 
-const adsElements = ads.map(({ author: { avatar }, offer: { title, address, price, type, rooms, guests, checkin, checkout, features, description, photos } }) => {
+
+const renderFeatureElements = (features) => {
+  const featureListFragment = document.createDocumentFragment();
+
+  features.forEach((feature) => {
+    const featureElement = document.createElement('li');
+    featureElement.classList.add('popup__feature', `popup__feature--${feature}`);
+    featureListFragment.appendChild(featureElement);
+  });
+
+  return featureListFragment;
+};
+
+
+const renderPhotoElements = (photos) => {
+  const photoListFragment = document.createDocumentFragment();
+
+  photos.forEach((photo) => {
+    const photoElement = document.createElement('img');
+    photoElement.classList.add('popup__photo');
+    photoElement.width = 45;
+    photoElement.height = 40;
+    photoElement.alt = 'Фотография жилья';
+    photoElement.src = photo;
+    photoListFragment.appendChild(photoElement);
+  });
+
+  return photoListFragment;
+};
+
+
+const renderAd = ({ author: { avatar }, offer: { title, address, price, type, rooms, guests, checkin, checkout, features, description, photos } }) => {
   const cardAdElement = cardAdTemplate.cloneNode(true);
-  const translateHousingType = (type) => {
-    switch (type) {
-      case 'flat':
-        return 'Квартира';
-      case 'bungalow':
-        return 'Бунгало';
-      case 'house':
-        return 'Дом';
-      case 'palace':
-        return 'Дворец';
-    }
-  }
 
   cardAdElement.querySelector('.popup__avatar').src = avatar;
   cardAdElement.querySelector('.popup__title').textContent = title;
@@ -27,48 +47,17 @@ const adsElements = ads.map(({ author: { avatar }, offer: { title, address, pric
   cardAdElement.querySelector('.popup__text--capacity').textContent = `${rooms} ${getDeclinedWord(rooms, ['комната', 'комнаты', 'комнат'])}
   для ${guests} ${getDeclinedWord(guests, ['гостя', 'гостей', 'гостей'])}`;
   cardAdElement.querySelector('.popup__text--time').textContent = `Заезд после ${checkin}, выезд до ${checkout}`;
-
-  const addFeatureElements = () => {
-    const featureListElement = cardAdElement.querySelector('.popup__features');
-    featureListElement.innerHTML = '';
-
-    const featureListFragment = document.createDocumentFragment();
-
-    features.forEach((feature) => {
-      const featureElement = document.createElement('li');
-      featureElement.classList.add('popup__feature', `popup__feature--${feature}`);
-      featureListFragment.appendChild(featureElement);
-    });
-
-    featureListElement.appendChild(featureListFragment);
-  };
-  addFeatureElements();
-
   cardAdElement.querySelector('.popup__description').textContent = description;
-  cardAdElement.querySelector('.popup__photos').textContent = description;
 
-  const addPhotoElements = () => {
-    const photoListElement = cardAdElement.querySelector('.popup__photos');
-    photoListElement.innerHTML = '';
+  cardAdElement.querySelector('.popup__features').innerHTML = '';
+  cardAdElement.querySelector('.popup__features').appendChild(renderFeatureElements(features))
 
-    const photoListFragment = document.createDocumentFragment();
-
-    photos.forEach((photo) => {
-      const photoElement = document.createElement('img');
-      photoElement.classList.add('popup__photo');
-      photoElement.width = 45;
-      photoElement.height = 40;
-      photoElement.alt = 'Фотография жилья';
-      photoElement.src = photo;
-      photoListFragment.appendChild(photoElement);
-    });
-
-    photoListElement.appendChild(photoListFragment);
-  };
-  addPhotoElements();
-
+  cardAdElement.querySelector('.popup__photos').innerHTML = '';
+  cardAdElement.querySelector('.popup__photos').appendChild(renderPhotoElements(photos));
 
   return cardAdElement;
-})
+}
+
+const adsElements = ads.map(renderAd);
 
 mapCanvas.appendChild(adsElements[3]);
--- js/util.js
+++ js/util.js
@@ -2,46 +2,60 @@ const getRandomInteger = (min, max) => {
   if (max >= min && min >= 0) {
     return Math.round(min - 0.5 + Math.random() * (max - min + 1));
   }
-
   if (max < min && max >= 0) {
     return Math.round(max - 0.5 + Math.random() * (min - max + 1));
   }
-
   throw new Error('Input data error');
 };
+
 
 const getRandomFractionNumber = (min, max, symbolNumber = 1) => {
   if (max >= min && min >= 0) {
     return +((min + Math.random() * (max - min)).toFixed(symbolNumber));
   }
-
   if (max < min && max >= 0) {
     return +((max + Math.random() * (min - max)).toFixed(symbolNumber));
   }
-
   throw new Error('Input data error');
 };
+
 
 const getRandomArrayElement = (elements) => {
   return elements[getRandomInteger(0, elements.length - 1)];
 };
+
 
 const getShuffleArray = (array) => {
   for (let i = array.length - 1; i > 0; i--) {
     let j = Math.floor(Math.random() * (i + 1));
     [array[i], array[j]] = [array[j], array[i]];
   }
-
   return array;
 };
+
 
 const getRandomArray = (array) => {
   return array.slice(0, getRandomInteger(1, array.length));
 };
+
 
 const getDeclinedWord = (number, words) => {
   return words[(number % 100 > 4 && number % 100 < 20) ? 2 : [2, 0, 1, 1, 1, 2][(number % 10 < 5) ? number % 10 : 5]];
 }
 
 
-export { getRandomInteger, getRandomFractionNumber, getRandomArrayElement, getShuffleArray, getRandomArray, getDeclinedWord };
+const translateHousingType = (type) => {
+  switch (type) {
+    case 'flat':
+      return 'Квартира';
+    case 'bungalow':
+      return 'Бунгало';
+    case 'house':
+      return 'Дом';
+    case 'palace':
+      return 'Дворец';
+  }
+}
+
+
+export { getRandomInteger, getRandomFractionNumber, getRandomArrayElement, getShuffleArray, getRandomArray, getDeclinedWord, translateHousingType };
